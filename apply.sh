11ea72bc674a6a30d8c5f26d58941f8385f245ee
geraldkan <geraldkan99@gmail.com>
2024-03-15 15:06:41 +0800

    added scripts to copy and paste from directories

---
+++ apply.sh
@@ -0,0 +1,4 @@
+cp ./zsh/.zshrc ~/.zshrc &&
+cp ./zsh/.p10k.zsh ~/.p10k.zsh &&
+cp ./nvim/.config/nvim/init.vim ~/.config/nvim/init.vim &&
+cp ./vim/.vimrc ~/.vimrc
---
+++ copy.sh
@@ -0,0 +1,5 @@
+cp ~/.zshrc ./zsh/.zshrc &&
+cp ~/.p10k.zsh ./zsh/.p10k.zsh &&
+cp ~/.config/nvim/init.vim ./nvim/.config/nvim/init.vim &&
+cp ~/.vimrc ./vim/.vimrc &&
+git diff
