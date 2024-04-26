37bb1ffada960829b5fbad28df6cf22363a27090
Ger4ldK <geraldkan99@gmail.com>
2024-04-26 11:25:57 +0800

    added Aerospace (MacOS i3-like WM) and config

--- apply.sh
+++ apply.sh
@@ -2,3 +2,4 @@ cp ./zsh/.zshrc ~/.zshrc &&
 cp ./zsh/.p10k.zsh ~/.p10k.zsh &&
 cp ./nvim/.config/nvim/init.vim ~/.config/nvim/init.vim &&
 cp ./vim/.vimrc ~/.vimrc
+cp ./aerospace/.aerospace.toml ~/.aerospace.toml
--- copy.sh
+++ copy.sh
@@ -2,4 +2,5 @@ cp ~/.zshrc ./zsh/.zshrc &&
 cp ~/.p10k.zsh ./zsh/.p10k.zsh &&
 cp ~/.config/nvim/init.vim ./nvim/.config/nvim/init.vim &&
 cp ~/.vimrc ./vim/.vimrc &&
+cp ~/.aerospace.toml ./aerospace/.aerospace.toml &&
 git diff
