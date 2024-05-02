56f03f0e11322985ac093bb8e01eae26795354d3
Ger4ldK <geraldkan99@gmail.com>
2024-05-02 23:13:18 +0800

    feat: moved alacritty config and updated files, added init script

--- apply.sh
+++ apply.sh
@@ -3,4 +3,4 @@ cp ./zsh/.p10k.zsh ~/.p10k.zsh &&
 cp ./nvim/.config/nvim/init.vim ~/.config/nvim/init.vim &&
 cp ./vim/.vimrc ~/.vimrc &&
 cp ./aerospace/.aerospace.toml ~/.aerospace.toml &&
-cp ./alacritty/.config/alacritty.toml ~/.config/alacritty.toml &&
+cp ./alacritty/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
--- copy.sh
+++ copy.sh
@@ -3,5 +3,5 @@ cp ~/.p10k.zsh ./zsh/.p10k.zsh &&
 cp ~/.config/nvim/init.vim ./nvim/.config/nvim/init.vim &&
 cp ~/.vimrc ./vim/.vimrc &&
 cp ~/.aerospace.toml ./aerospace/.aerospace.toml &&
-cp ~/.config/alacritty/alacritty.toml ./alacritty/.config/alacritty.toml &&
+cp ~/.config/alacritty/alacritty.toml ./alacritty/.config/alacritty/alacritty.toml &&
 git diff
