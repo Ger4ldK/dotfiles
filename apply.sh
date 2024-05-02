1466244c5737441bb21f14506f8809512b2bf8a4
Ger4ldK <geraldkan99@gmail.com>
2024-05-02 21:23:03 +0800

    feat: added basic alacritty config

--- apply.sh
+++ apply.sh
@@ -1,5 +1,6 @@
 cp ./zsh/.zshrc ~/.zshrc &&
 cp ./zsh/.p10k.zsh ~/.p10k.zsh &&
 cp ./nvim/.config/nvim/init.vim ~/.config/nvim/init.vim &&
-cp ./vim/.vimrc ~/.vimrc
-cp ./aerospace/.aerospace.toml ~/.aerospace.toml
+cp ./vim/.vimrc ~/.vimrc &&
+cp ./aerospace/.aerospace.toml ~/.aerospace.toml &&
+cp ./alacritty/.config/alacritty.toml ~/.config/alacritty.toml &&
--- copy.sh
+++ copy.sh
@@ -3,4 +3,5 @@ cp ~/.p10k.zsh ./zsh/.p10k.zsh &&
 cp ~/.config/nvim/init.vim ./nvim/.config/nvim/init.vim &&
 cp ~/.vimrc ./vim/.vimrc &&
 cp ~/.aerospace.toml ./aerospace/.aerospace.toml &&
+cp ~/.config/alacritty/alacritty.toml ./alacritty/.config/alacritty.toml &&
 git diff
