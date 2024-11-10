45feef0a416927322ef630b61c26a8d7981877fb
Ger4ldK <geraldkan99@gmail.com>
2024-11-10 11:13:21 +0800

    fix: copy and apply scripts

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 cp ./zsh/.zshrc ~/.zshrc &&
 cp ./zsh/.p10k.zsh ~/.p10k.zsh &&
-cp -r ./nvim/.config/nvim/* ~/.config/nvim/ &&
+cp -r ./nvim/.config/nvim/ ~/.config/nvim/ &&
 cp ./vim/.vimrc ~/.vimrc &&
 cp ./aerospace/.aerospace.toml ~/.aerospace.toml &&
 cp ./wezterm/.wezterm.lua ~/.wezterm.lua
--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 cp ~/.zshrc ./zsh/.zshrc &&
 cp ~/.p10k.zsh ./zsh/.p10k.zsh &&
-cp -r ~/.config/nvim/* ./nvim/.config/nvim/* &&
+cp -r ~/.config/nvim/ ./nvim/.config/nvim/ &&
 cp ~/.vimrc ./vim/.vimrc &&
 cp ~/.aerospace.toml ./aerospace/.aerospace.toml &&
 cp ~/.wezterm.lua ./wezterm/.wezterm.lua &&
