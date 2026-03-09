7cedafa16ff81f9d8ae6cc0dd1ee0b75ee2f9ca1
Ger4ldK <geraldkan99@gmail.com>
2026-03-09 12:36:09 +0800

    (feat) update niri wallpaper config

--- cwal/script.sh
+++ cwal/script.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
+
+cwalOutput="$HOME/.config/cwal/out/"
+
 # ownership (ideally by group) needs to be granted to $2 using chown, else script needs sudo on the lines to copy configs
 checkAndCopy () {
-    if ! [ -f "$2" ]; then
+    if ![ -f "$2" ]; then
         touch "$2"
     fi
-    cp -f "$HOME/.config/cwal/out/$1" "$2"
+    cp -f "$1" "$2"
 }	
 
-checkAndCopy "colors-sysc-greet.toml" "/usr/share/sysc-greet/themes/colors-sysc-greet.toml"
-checkAndCopy "colors-fuzzel.ini" "$HOME/.config/fuzzel/colors-fuzzel.ini"
+checkAndCopy "${cwalOutput}colors-sysc-greet.toml" "/usr/share/sysc-greet/themes/colors-sysc-greet.toml"
+checkAndCopy "${cwalOutput}colors-fuzzel.ini" "$HOME/.config/fuzzel/colors-fuzzel.ini"
+checkAndCopy "${cwalOutput}colors-niri.kdl" "$HOME/.config/niri/colors-niri.kdl"
