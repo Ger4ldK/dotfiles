9056594c1f1cefd1a8eea1d964f7fe099ed32de3
Ger4ldK <geraldkan99@gmail.com>
2026-03-09 12:04:53 +0800

    (chore) begin, again

--- copy.sh
+++
@@ -1,7 +0,0 @@
-cp ~/.zshrc ./zsh/.zshrc &&
-cp ~/.p10k.zsh ./zsh/.p10k.zsh &&
-cp -r ~/.config/nvim/ ./nvim/.config/nvim/ &&
-cp ~/.vimrc ./vim/.vimrc &&
-cp ~/.aerospace.toml ./aerospace/.aerospace.toml &&
-cp ~/.wezterm.lua ./wezterm/.wezterm.lua &&
-git diff
--- wezterm/.wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wt = require 'wezterm'
-local config = wt.config_builder()
-
-config.color_scheme = 'Atelier Forest (base16)'
-config.font = wt.font('FiraCode Nerd Font')
-config.font_size = 15
-config.freetype_load_flags = 'NO_HINTING'
-
-return config
