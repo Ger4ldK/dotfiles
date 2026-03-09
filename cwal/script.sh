18eca06a9e091a1af20042aaec5b07d027af455a
Ger4ldK <geraldkan99@gmail.com>
2026-03-09 13:03:33 +0800

    (feat) alacritty color config

--- cwal/script.sh
+++ cwal/script.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-cwalOutput="$HOME/.config/cwal/out/"
-
 # ownership (ideally by group) needs to be granted to $2 using chown, else script needs sudo on the lines to copy configs
 checkAndCopy () {
     if ![ -f "$2" ]; then
@@ -10,7 +8,7 @@ checkAndCopy () {
     cp -f "$1" "$2"
 }	
 
-checkAndCopy "${cwalOutput}colors-sysc-greet.toml" "/usr/share/sysc-greet/themes/colors-sysc-greet.toml"
-checkAndCopy "${cwalOutput}colors-fuzzel.ini" "$HOME/.config/fuzzel/colors-fuzzel.ini"
-checkAndCopy "${cwalOutput}colors-niri.kdl" "$HOME/.config/niri/colors-niri.kdl"
-checkAndCopy "${cwalOutput}colors-alacritty.toml" "$HOME/.config/alacritty/colors-alacritty.toml"
+checkAndCopy "${CWAL_OUT_DIR}/colors-sysc-greet.toml" "/usr/share/sysc-greet/themes/colors-sysc-greet.toml"
+checkAndCopy "${CWAL_OUT_DIR}/colors-fuzzel.ini" "$HOME/.config/fuzzel/colors-fuzzel.ini"
+checkAndCopy "${CWAL_OUT_DIR}/colors-niri.kdl" "$HOME/.config/niri/colors-niri.kdl"
+checkAndCopy "${CWAL_OUT_DIR}/colors-alacritty.toml" "$HOME/.config/alacritty/colors-alacritty.toml"
